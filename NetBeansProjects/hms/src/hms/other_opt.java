4f7f5817ed169af0de9251a7d9a3778b4fb814bf
Ezhil <ezhil99mutsun@gmail.com>
2019-09-08 16:04:25 +0530

    ward and icu done

--- NetBeansProjects/hms/src/hms/other_opt.java
+++ NetBeansProjects/hms/src/hms/other_opt.java
@@ -9,12 +9,12 @@ package hms;
  *
  * @author ezhilnero
  */
-public class dept_admin extends javax.swing.JFrame {
+public class other_opt extends javax.swing.JFrame {
 
     /**
-     * Creates new form dept_admin
+     * Creates new form other_opt
      */
-    public dept_admin() {
+    public other_opt() {
         initComponents();
     }
 
@@ -31,117 +31,77 @@ public class dept_admin extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
-        patient_btn = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
-        jLabel2 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(770, 454));
         setResizable(false);
-        setSize(new java.awt.Dimension(774, 455));
 
         jPanel1.setLayout(null);
 
-        jButton1.setFont(new java.awt.Font("Noto Serif CJK TC", 1, 14)); // NOI18N
-        jButton1.setText("Blood Bank");
+        jButton1.setFont(new java.awt.Font("Noto Serif CJK TC", 1, 16)); // NOI18N
+        jButton1.setText("GENERAL WARD");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
         jPanel1.add(jButton1);
-        jButton1.setBounds(480, 190, 120, 31);
+        jButton1.setBounds(470, 210, 170, 50);
 
-        jButton2.setFont(new java.awt.Font("Noto Serif CJK TC", 1, 14)); // NOI18N
-        jButton2.setText("Laboratory");
+        jButton2.setFont(new java.awt.Font("Noto Serif CJK TC", 1, 16)); // NOI18N
+        jButton2.setText("ICU");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton2ActionPerformed(evt);
             }
         });
         jPanel1.add(jButton2);
-        jButton2.setBounds(140, 370, 150, 31);
+        jButton2.setBounds(140, 220, 130, 50);
 
-        jButton3.setFont(new java.awt.Font("Noto Serif CJK TC", 1, 14)); // NOI18N
-        jButton3.setText("OTHER");
+        jButton3.setText("BACK");
         jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton3ActionPerformed(evt);
             }
         });
         jPanel1.add(jButton3);
-        jButton3.setBounds(490, 360, 120, 31);
+        jButton3.setBounds(340, 340, 80, 50);
 
-        patient_btn.setFont(new java.awt.Font("Noto Serif CJK TC", 1, 14)); // NOI18N
-        patient_btn.setText("Patient");
-        patient_btn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                patient_btnActionPerformed(evt);
-            }
-        });
-        jPanel1.add(patient_btn);
-        patient_btn.setBounds(160, 190, 100, 31);
-
-        jButton5.setText("Back");
-        jButton5.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton5ActionPerformed(evt);
-            }
-        });
-        jPanel1.add(jButton5);
-        jButton5.setBounds(330, 410, 90, 31);
-
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/hms/back4.png"))); // NOI18N
-        jPanel1.add(jLabel2);
-        jLabel2.setBounds(0, 0, 770, 450);
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/hms/bed1.png"))); // NOI18N
+        jPanel1.add(jLabel1);
+        jLabel1.setBounds(0, 0, 760, 440);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 767, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 758, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 449, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 437, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void patient_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_patient_btnActionPerformed
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
 super.dispose();
-new patient_depadm().setVisible(true);
-        // TODO add your handling code here:
-    }//GEN-LAST:event_patient_btnActionPerformed
-
-    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
-        super.dispose();
-        new login_page().setVisible(true);
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton5ActionPerformed
+new icu().setVisible(true);
+// TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-super.dispose();
-new bloodbank().setVisible(true);
-
+      super.dispose();
+new gen_ward().setVisible(true);
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-super.dispose();
-new lab_opt().setVisible(true);
-
-
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
-
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
 super.dispose();
-new other_opt().setVisible(true);
+new dept_admin().setVisible(true);        
 
-
-        // TODO add your handling code here:
+// TODO add your handling code here:
     }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
@@ -161,20 +121,20 @@ new other_opt().setVisible(true);
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(dept_admin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(other_opt.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(dept_admin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(other_opt.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(dept_admin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(other_opt.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(dept_admin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(other_opt.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new dept_admin().setVisible(true);
+                new other_opt().setVisible(true);
             }
         });
     }
@@ -183,9 +143,7 @@ new other_opt().setVisible(true);
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton5;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JButton patient_btn;
     // End of variables declaration//GEN-END:variables
 }
